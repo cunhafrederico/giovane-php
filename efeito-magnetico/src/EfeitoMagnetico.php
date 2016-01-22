c907cb3b709e0ee9f34d1cf8d6044b9616642336
giovane <giovane@vialink.com.br>
2016-01-22 07:46:28 -0200

    Faltando ponto e vírgula

--- efeito-magnetico/src/EfeitoMagnetico.php
+++ efeito-magnetico/src/EfeitoMagnetico.php
@@ -92,7 +92,7 @@ class EfeitoMagnetico {
 
 	public function calculaDistancia($x1, $y1, $x2, $y2)
 	{
-		return sqrt(pow($x1 - $x2, 2) + pow($y1 - $y2, 2))
+		return sqrt(pow($x1 - $x2, 2) + pow($y1 - $y2, 2));
 	}
 
 }
