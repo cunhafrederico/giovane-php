4b4a929abb179bef2614dd09448e016815b40c11
giovane <giovane@vialink.com.br>
2016-01-22 04:01:19 -0200

    Criando um primeiro teste que roda

---
+++ ano-bissexto/tests/AnoBissextoTest.php
@@ -0,0 +1,12 @@
+<?php
+class AnoBissextoTest extends PHPUnit_Framework_TestCase
+{
+    // Criando um primeiro teste que passa
+
+    public function testTest()
+    {
+        $one = 1;
+        $this->assertEquals(1, $one);
+    }
+
+}
