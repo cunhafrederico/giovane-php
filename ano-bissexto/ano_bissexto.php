1362a5f22c727bab78f6d9699a3e911d31188a8f
giovane <giovane@vialink.com.br>
2016-01-22 05:33:23 -0200

    Adicionando o tempo de execução

--- ano-bissexto/ano_bissexto.php
+++ ano-bissexto/ano_bissexto.php
@@ -1,8 +1,16 @@
 #!/usr/bin/php5
 <?php
 
+$start = microtime(true);
+
 require_once 'src/AnoBissexto.php';
 
 AnoBissexto::ehBissexto($argv[1]);
+
+echo PHP_EOL;
+
+$time_elapsed_secs = microtime(true) - $start;
+
+printf('Tempo de execução: %fs.', $time_elapsed_secs);
 
 echo PHP_EOL;
