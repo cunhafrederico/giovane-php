bed417cadbee4d893bda3aaff25b9f4adc290ccc
giovane <giovane@vialink.com.br>
2016-01-22 06:45:14 -0200

    Diretório tests

---
+++ efeito-magnetico/tests/EfeitoMagneticoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class EfeitoMagneticoTest extends PHPUnit_Framework_TestCase
+{
+
+	// Fazendo teste que passa
+
+	public function testTest()
+	{
+		$one = 1;
+		$this->assertEquals(1, $one);
+	}
+}
