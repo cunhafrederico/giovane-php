420dc0170297688a18bab0a4ce93510e70806181
giovane <giovane@vialink.com.br>
2016-01-22 09:29:57 -0200

    Melhorando o autoload pra evitar warnings desnecessários

--- ano-bissexto/src/autoload.php
+++ ano-bissexto/src/autoload.php
@@ -1,5 +1,8 @@
 <?php
 
 spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
+	$file = $class_name . '.php';
+	if (file_exists($file)) {
+		include $class_name . '.php';
+	}
 });
