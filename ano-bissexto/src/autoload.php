4ebf67c0ce9317472d75d6a18c70f049e1c6dc2a
giovane <giovane@vialink.com.br>
2016-01-22 09:37:58 -0200

    Corrigindo o broke build no semaphore

--- ano-bissexto/src/autoload.php
+++ ano-bissexto/src/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class_name) {
-	$file = $class_name . '.php';
+	$file = dirname(__FILE__) . $class_name . '.php';
 	if (file_exists($file)) {
 		include $class_name . '.php';
 	}
